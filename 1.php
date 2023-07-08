3ed4a0aca22c52b4a64e759e1732bc09286d467e
Het Shah <75979397+shahhet122@users.noreply.github.com>
2023-07-08 17:05:04 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>1.php</title>
+</head>
+<body>
+  welcome, <?php 
+    echo $_POST["name"];
+    echo "<br>";
+    echo $_POST["email"];
+    echo "<br>";
+    echo $_POST["password"];
+    echo "<br>";
+    echo $_POST["phone"];
+ 
+    ?>
+</body>
+</html>
